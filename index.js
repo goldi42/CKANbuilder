0efc1c9227c680958fe3d70f3c9d53c0cba8a10d
Markus Goldbach <markus.goldbach@dkd.de>
2017-07-04 13:26:55 +0200

    add installation command

--- index.js
+++ index.js
@@ -23,5 +23,25 @@ program
             break;
         }
     });
+program
+    .command('install <task>')
+    .description('install ckan or all extensions for ckan. Valid tasks are \'ckan\' and \'extensions\' ')
+    .option('-j, --json_file [file]','JSON File with ckan extensions', path.join(process.cwd(), 'config', 'extensions.json'))
+    .option('-i, --install_dir [path]','directory for installation', path.join(process.cwd(), 'extension'))
+    .option('-c, --ckan_version [version]', 'ckan version which should be installed', '2.5.5')
+    .action( (task, command) => {
+        switch (task) {
+        case 'extensions':
+            var extensionInstaller = require('./lib/install/extensions');
+            extensionInstaller.install(command.json_file, command.install_dir);
+            break;
+        case 'ckan':
+            var ckanInstaller = require('./lib/install/ckan');
+            ckanInstaller.install(command.ckan_version, command.install_dir);
+            break;
+        default:
+            break;
+        }
+    });
 
 program.parse(process.argv);
