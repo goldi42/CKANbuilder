86d1a047642339a7112050538ee77d3e1a01078b
Markus Goldbach <markus.goldbach@dkd.de>
2017-08-09 16:47:40 +0200

    update the ckanbuilder env on each command centralize ckanncofig_path option

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const CkanJsonParser = require('./lib/utils/ckanJsonParser');
+const LogService = require('./lib/logging/logService');
+const pathUtil = require('./lib/utils/path');
 
 const BuildCommand = require('./command/build');
 const InstallCommand = require('./command/install');
@@ -10,26 +12,26 @@ let program = require('commander');
 
 program
     .version('1.0.0')
-    .description('ckan build tool for common tasks');
+    .description('ckan build tool for common tasks')
+    .option('-l, --file_log','activate the filelog', false)
+    .option('-c, --ckanconfig_path [path]','Path to Ckan.json File with ckan config');
 
 program
     .command('build <task>')
     .description('build operations')
-    .option('-c, --ckanconfig_path [path]','Path to Ckan.json File with ckan config')
     .option('-e, --extension_path [path]', 'Path where the extension are saved')
     .action((task, command) => {
-        updateCkanConfigPath(command);
+        updateEnvironment(this.parent);
         let buildCommand = new BuildCommand(task, command);
         buildCommand.exec();
     });
 program
     .command('install <task>')
     .description('install ckan or all extensions for ckan. Valid tasks are \'ckan\' and \'extensions\' ')
-    .option('-c, --ckanconfig_path [path]','Path to Ckan.json File with ckan config')
     .option('-d, --install_dir [path]','directory for installation')
     .option('-i, --ckan_version [version]', 'ckan version which should be installed')
     .action( (task, command) => {
-        updateCkanConfigPath(command);
+        updateEnvironment(command.parent);
         let installCommand = new InstallCommand(task, command);
         installCommand.exec();
     });
@@ -37,11 +39,10 @@ program
 program
     .command('download <task>')
     .description('download ckan or all extensions')
-    .option('-c, --ckanconfig_path [path]','Path to Ckan.json File with ckan config')
     .option('-d, --install_dir [path]','directory for installation')
     .option('-i, --ckan_version [version]', 'ckan version which should be installed', '2.5.5')
     .action( (task, command) => {
-        updateCkanConfigPath(command);
+        updateEnvironment(command.parent);
         let downloadCommand = new DownloadCommand(task, command);
         downloadCommand.exec();
     });
@@ -49,9 +50,8 @@ program
 program
     .command('configure <task>')
     .option('-i, --configini_file <file>','Ckan config ini')
-    .option('-c, --ckanconfig_path [path]','Path to Ckan.json File with ckan config')
     .action( (task, command) => {
-        updateCkanConfigPath(command);
+        updateEnvironment(command.parent);
         let configureCommand = new ConfigureCommand(task,command);
         configureCommand.exec();
     });
@@ -59,13 +59,18 @@ program
 program
     .command('generate <task>')
     .action( (task, command) => {
+        updateEnvironment(command.parent);
         let generateCommand = new GenerateCommand(task, command);
         generateCommand.exec();
     });
 program.parse(process.argv);
 
-function updateCkanConfigPath (command) {
-    if (command.ckanconfig_path) {
-        CkanJsonParser.getInstance().directory = command.ckanconfig_path;
+function updateEnvironment(program) {
+    if (program.ckanconfig_path) {
+        CkanJsonParser.getInstance().directory = program.ckanconfig_path;
+    }
+
+    if (program.file_log) {
+        LogService.getInstance().initializeFileLog(CkanJsonParser.getInstance().ckanJson.filelog, pathUtil.getComponentDirectory('logs', CkanJsonParser.getInstance().components));
     }
 }
